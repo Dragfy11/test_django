d5fe7009807ce0cddf221e97486ee946ae45bbb7
Dragfy11 <dragfy_dark@hotmail.com>
2021-05-07 14:40:54 +0200

    nom article admin

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,7 @@ class Article(models.Model):
     body = models.TextField()
     Author = models.CharField(max_length=200)
     date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
