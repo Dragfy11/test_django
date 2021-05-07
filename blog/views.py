c53a7fb8bb3c358bb094de3784ce80ad7fb56401
Dragfy11 <dragfy_dark@hotmail.com>
2021-05-07 14:58:15 +0200

    liste article

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 def blog_index(request):
-    return render(request, 'blog/blog_index.html')
+    articles = Article.objects.all()
+    data = {'articles': articles}
+    return render(request, 'blog/blog_index.html', data)
 
 def article(request, name):
     return render(request, 'blog/article.html')
